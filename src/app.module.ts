f014d444eb41a8abf28e6a7be780965c50ea6b56
Igor Titov <god19842@mail.ru>
2024-04-03 20:12:32 +0300

    add lesson module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from '@auth/guards/jwt-auth.guard';
+import { LessonModule } from './lesson/lesson.module';
 
 @Module({
-    imports: [UserModule, PrismaModule, AuthModule, ConfigModule.forRoot({ isGlobal: true })],
+    imports: [UserModule, PrismaModule, AuthModule, ConfigModule.forRoot({ isGlobal: true }), LessonModule],
     providers: [
         {
             provide: APP_GUARD,
---
+++ src/lesson/lesson.controller.ts
@@ -0,0 +1,15 @@
+import { Delete, Get, Post, Controller } from '@nestjs/common';
+
+import { LessonService } from './lesson.service';
+import { JwtPayload } from '@auth/interfaces';
+import { CurrentUser } from '@common/decorators';
+
+@Controller('lesson')
+export class LessonController {
+    constructor(private readonly lessonService: LessonService) {}
+
+    @Get()
+    getLesson(@CurrentUser() user: JwtPayload) {
+        return this.lessonService.findLesson(user);
+    }
+}
