ada3cc0f65e825fba18b16423f8c52611e7940ec
server <server@mail.ru>
2024-04-20 15:51:45 +0300

    add cors and some fix

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.use(cookieParser());
     app.setGlobalPrefix('api');
+    app.enableCors();
 
     const config = new DocumentBuilder()
         .setTitle('Personal cabinet')
--- src/specializations/specializations.controller.ts
+++ src/specializations/specializations.controller.ts
@@ -5,15 +5,15 @@ import { CreateSpecializationDto, UpdateSpecializationDto } from './dto';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { RolesGuard } from '@auth/guards/role.guard';
 import { Role } from '@prisma/client';
+import { Public } from "@common/decorators";
 
 @ApiTags('Specialization')
 @Controller('specializations')
 @ApiBearerAuth('JWT-auth')
+@Public()
 export class SpecializationsController {
     constructor(private readonly specializationsService: SpecializationsService) {}
 
-    @UseGuards(RolesGuard)
-    @Roles(Role.RESOURCES_DEPARTMENT)
     @Get()
     getSpecializations() {
         return this.specializationsService.findAll();
